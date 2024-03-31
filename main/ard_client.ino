a343c32d70dacd51946d3773ce8c6e3b8a2a1c14
Eugene12879 <eugene12879@gmail.com>
2024-03-31 17:47:10 +0900

    commit

--- main/ard_client.ino
+++ main/ard_client.ino
@@ -1,10 +1,6 @@
-'''
-Arduino Client : 아두이노 기기제어
-작성일 : 2024.03.30
-'''
-#include <SoftwareSerial.h> // 시리얼 통신
-#include <Wire.h> // I2C 통신
-#include <Servo.h> // 서보모터
+#include <SoftwareSerial.h>
+#include <Wire.h>
+#include <Servo.h> // 서보모터 라이브러리 추가
 
 #define DEBUG
 #define IRSENSOR1_PIN 2 // 적외선 센서 핀
@@ -15,19 +11,17 @@ Arduino Client : 아두이노 기기제어
 #define MOTOR1_PIN 7
 #define MOTOR2_PIN 8
 #define MOTOR3_PIN 9
-#define LED_BUILTIN_PIN 13
 
 #define ARR_CNT 5
 #define CMD_SIZE 60
 
+char sendBuf[CMD_SIZE];
+char recvId[10] = "SQL"; // SQL 저장 클라이언트 ID
 
-char sendBuf[CMD_SIZE];   // Bluetooeh Data Buffer
-char recvId[10] = "SQL";  // SQL 저장 클라이언트 ID
+bool Flag = false;
+SoftwareSerial BTSerial(10, 11); 
 
-bool timerIsrFlag = false;
-unsigned int secCount;
-SoftwareSerial BTSerial(10, 11); // RX, TX
-
+// 상승, 하강엣지 탐지를 위한 previous, currnet value 변수 지정
 int previousIrValue1 = 0;
 int currentIrValue1 = 0;
 int previousIrValue2 = 0;
@@ -37,53 +31,58 @@ int currentIrValue3 = 0;
 int previousIrValue4 = 0;
 int currentIrValue4 = 0;
 
-int doorstate = 0;  // 문 상태
+int doorstate = 0;
 
-Servo myServo1;     // 서보모터 객체 생성
+
+Servo myServo1; // 서보모터 객체 생성
 
 void setup() {
 #ifdef DEBUG
+
   Serial.begin(115200);
   Serial.println("setup() start!");
+
 #endif
 
-  pinMode(LED_BUILTIN_PIN, OUTPUT); // LED_BUILTIN 핀을 출력으로 설정
   pinMode(IRSENSOR1_PIN, INPUT);
   pinMode(IRSENSOR2_PIN, INPUT);
   pinMode(IRSENSOR3_PIN, INPUT);
   pinMode(IRSENSOR4_PIN, INPUT);
+
   pinMode(MOTOR1_PIN, OUTPUT);
   pinMode(MOTOR2_PIN, OUTPUT);
   pinMode(MOTOR3_PIN, OUTPUT);
 
-  digitalWrite(LED_BUILTIN_PIN, LOW);
+  
   digitalWrite(MOTOR1_PIN, LOW);
   digitalWrite(MOTOR2_PIN, LOW);
   digitalWrite(MOTOR3_PIN, LOW);
 
   BTSerial.begin(9600); // 블루투스 시리얼 통신 설정
 
-  myServo1.attach(12);  // 서보모터 핀 설정
+  myServo1.attach(12); // 서보모터 핀 설정
+  myServo1.write(0); 
+
 }
 
 void loop() {
   if (BTSerial.available())
     bluetoothEvent();
 
-  previousIrValue1 = currentIrValue1;
+  previousIrValue1 = currentIrValue1; 
   previousIrValue2 = currentIrValue2;
   previousIrValue3 = currentIrValue3;
   previousIrValue4 = currentIrValue4;
 
-  currentIrValue1 = digitalRead(IRSENSOR1_PIN);
-  currentIrValue2 = digitalRead(IRSENSOR2_PIN);
-  currentIrValue3 = digitalRead(IRSENSOR3_PIN);
-  currentIrValue4 = digitalRead(IRSENSOR4_PIN);
+  currentIrValue1 = digitalRead(IRSENSOR1_PIN); 
+  currentIrValue2 = digitalRead(IRSENSOR2_PIN); 
+  currentIrValue3 = digitalRead(IRSENSOR3_PIN); 
+  currentIrValue4 = digitalRead(IRSENSOR4_PIN); 
 
-  if (timerIsrFlag) {
+  if (Flag) {
 
+    // 각 적외선 센서의 출력값의 상승, 하강엣지를 감지해 모터를 제어
 
-    // 각 적외선 센서와 해당 모터를 세트로 연결하여 물체가 감지되면 해당 모터를 멈추도록 설정
     if (digitalRead(IRSENSOR1_PIN) == LOW) {
       digitalWrite(MOTOR1_PIN, LOW);
     } else {
@@ -103,34 +102,36 @@ void loop() {
     }
 
     if (digitalRead(IRSENSOR4_PIN) == HIGH) {
-      digitalWrite(12, LOW);
+      myServo1.write(0);
     } else {
-      digitalWrite(12, HIGH);
+      myServo1.write(110);
     }
+
   }
 
-  // 적외선 센서의 상태 변화를 감지하여 모터를 멈추는 코드 추가
-  if (previousIrValue1 == LOW && currentIrValue1 == HIGH) {
-    digitalWrite(MOTOR1_PIN, LOW); // 모터1를 멈추는 코드
+  // 적외선 센서의 출력값에 따른 해당 모터의 기본 상태를 설정
+  
+  if (previousIrValue1 == LOW && currentIrValue1 == HIGH) { 
+    digitalWrite(MOTOR1_PIN, LOW); 
   }
 
   if (previousIrValue2 == LOW && currentIrValue2 == HIGH) {
-    digitalWrite(MOTOR2_PIN, LOW); // 모터2를 멈추는 코드
+    digitalWrite(MOTOR2_PIN, LOW); 
   }
 
   if (previousIrValue3 == LOW && currentIrValue3 == HIGH) {
-    digitalWrite(MOTOR3_PIN, LOW); // 모터3를 멈추는 코드
+    digitalWrite(MOTOR3_PIN, LOW); 
   }
 
-  //서보모터1 자동 개폐
   if (doorstate == 1 && previousIrValue4 == HIGH && currentIrValue4 == LOW) {
-     delay(5000);
-     doorstate = 1;
+     delay(7000); 
+     doorstate = 1; 
    }
-
+   
+   
   if (doorstate == 1 && previousIrValue4 == LOW && currentIrValue4 == HIGH) {
-    delay(3000);
-    doorstate = 0;
+    delay(7000); 
+    doorstate = 0; 
     myServo1.write(0);
   }
 
@@ -140,7 +141,8 @@ void loop() {
 #endif
 }
 
-void bluetoothEvent() {
+//블루투스 모듈을 통한 명령어 송신, 선택된 모터 제어
+void bluetoothEvent() { 
   int i = 0;
   char *pToken;
   char *pArray[ARR_CNT] = {0};
@@ -159,6 +161,8 @@ void bluetoothEvent() {
       break;
     pToken = strtok(NULL, "[@]");
   }
+
+  // 디버그를 위한 off 명령어 추가
 
  if (!strcmp(pArray[1], "MOTOR1")) { // 모터1 제어 명령
     if (!strcmp(pArray[2], "ON")) {
@@ -183,11 +187,10 @@ void bluetoothEvent() {
     sprintf(sendBuf, "[%s]%s@%s\n", pArray[0], pArray[1], pArray[2]);
   } else if (!strcmp(pArray[1], "SERVO")) { // 서보모터 제어 명령
     if (!strcmp(pArray[2], "ON")) {
-      // 서보모터를 같은 각도로 회전
       doorstate = 1;
-      myServo1.write(110); // 예: 110도 회전
-    } else if (!strcmp(pArray[2], "OFF")) {
-      // 서보모터를 원위치로 회전
+      myServo1.write(110); // 110도 회전
+    } 
+    else if (!strcmp(pArray[2], "OFF")) { 
       doorstate = 0;
       myServo1.write(0); // 원위치로 회전
     }
